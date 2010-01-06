bb7298b776c62fb1d3f5ea766d2ad04af31b0021
timv@herman <none@none>
2010-01-06 12:51:09 -0500

    added a baseline date pattern and some tests for it.

--- nlp/patterns.py
+++ nlp/patterns.py
@@ -55,3 +55,98 @@ EMAIL_RE = re.compile("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}
 
 # This pattern is more for validation
 EMAIL_STRICT = re.compile('[A-Z0-9._%+-]+@[A-Z0-9.-]+\.(?:[A-Z]{2}|com|org|net|gov|mil|biz|info|mobi|name|aero|jobs|museum)', re.IGNORECASE)
+
+
+DATE_RE = re.compile("""
+(
+    # optional: day of the week
+    (?:
+        (?:mon
+            |tues?
+            |wed(?:nes)?
+            |thurs?
+            |fri
+            |satu?r?
+            |sun
+        )(?:day|\.?)
+    )?
+
+    \s* ,? \s*?
+
+    (?:
+        (?:the\s+)?
+        [0-3]?[0-9]
+        (?:st|nd|rd|th|)
+        ( \s*? of )?
+        \s*?
+    )?
+
+    # mandatory: month (written)
+    (?:\s+?|^)
+    (?:jan(?:uary)?
+        |febr?(?:uary)?
+        |mar(?:ch)?
+        |apr(?:il)?
+        |may
+        |june?
+        |july?
+        |aug(?:ust)?
+        |sept?(?:ember)?
+        |oct(?:ober)?
+        |nov(?:ember)?
+        |dec(?:ember)?
+    )
+
+    (?: \. | , |) \s+
+
+    ( [0-3][0-9] | [0-9] )?  (?:st|nd|rd|th|\s|$)   # the word splitter will always keep numbers ord-suffix together
+
+    \s*
+    (?: of | , | ) \s+
+    
+    (  [0-9][0-9][0-9][0-9] | [0-9][0-9] )?
+
+)
+""", re.VERBOSE|re.IGNORECASE)
+
+
+if __name__ == '__main__':
+
+    from wordsplitter import wordsplit_sentence
+
+    def parse_text(text):
+        return wordsplit_sentence(text).split()
+
+    def remove_extra_spaces(s):
+        return re.sub('\s+', ' ', s)
+
+    def with_extra_spaces(seq):
+        return '       '.join(seq)
+
+    def test_sentence(s, target=''):
+        print
+        seq = parse_text(s)
+        print 'input: ', ' '.join(seq)
+        output = remove_extra_spaces(DATE_RE.findall(with_extra_spaces(seq))[0][0])
+        print 'output:', output.strip()
+        print 'target:', target.strip()
+        if target:
+            assert target.strip() == output.strip()
+        return seq
+
+    test_sentence('I was born on Monday, March of 1986.',  'Monday , March of 1986')
+    test_sentence('I was born on Monday, March 18, 1986.', 'Monday , March 18 , 1986')
+    test_sentence('I was born on March 18 1986 .',         'March 18 1986')
+    test_sentence('I was born on Mon. March 18, 86 .',     'Mon. March 18 , 86')
+    test_sentence('I was born on March 18, 86 .',          'March 18 , 86')
+    test_sentence('I was born on March, 86 .',             'March , 86')
+    test_sentence('I was born on March 86 .',              'March 86')
+    test_sentence('I was born on Mon. March, 1986 .',      'Mon. March , 1986')
+    test_sentence('11th February 1990 , ',                 '11th February 1990')
+    test_sentence('the 11th of February , 1990 , ',        'the 11th of February , 1990')
+    test_sentence('February, 1990 , ',                     'February , 1990')
+    test_sentence('Monday the 3rd of February, 1990 , ',   'Monday the 3rd of February , 1990')
+    test_sentence('Mon. the 3rd of February, 1990 , ',     'Mon. the 3rd of February , 1990')
+    test_sentence('Mon. 3rd of February, 1990 , ',         'Mon. 3rd of February , 1990')
+    test_sentence('Mon. 30th of February 1990 ',           'Mon. 30th of February 1990')
+    test_sentence('th February 1990 , ',                   'February 1990')

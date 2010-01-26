cbe208214f201c5046b1d00433ff2e46334726de
timv@herman <none@none>
2010-01-26 14:59:41 -0500

    i added a bit to the weirdness of class definitions python snippet

--- python-snippets/weirdness-of-class-definitions.py
+++ python-snippets/weirdness-of-class-definitions.py
@@ -1,34 +1,62 @@
+## this on is really weird...
+class A:
+    a = 42
+
+    # how is it that this works:
+    b = a + 10
+
+    c = (a + i for i in range(10))
+
+    # but this doesn't?
+    try:
+        c.next()
+    except NameError:
+        pass
+    else:
+        raise AssertionError
+
+    # but whats so different from this one?
+    d = [a + i for i in range(10)]
+
+    # The Reason:
+    #   The scope of names defined in a class block is limited to the 
+    #   class block; it does not extend to the code blocks of methods 
+    #   -- this includes generator expressions since they are implemented 
+    #   using a *function scope*.
 
 
-##class Foo:
-##
-##    print 'making class Foo.'
-##
-##    x = 10
-##
-##    for i in xrange(10):
-##
-##        def goo(self):
-##            #print self.x
-##            #print self.__class__.x
-##            #print Foo.x
-##
-##            xxx = i
-##            print xxx
-##
-##
-##        class GOOF:
-##            print 'trying to make GOOF class...'
-##            #xxx = i
-##            #print 'GOOF:', xxx
-##            print i
-##            print 'done making GOOD class'
-##        
-##        #print 'GOOF.xxx:', GOOF.xxx
-##
-##        print 'i:', i, goo
-##
-##    print 'end making class Foo'
+# A scope defines the visibility of a name within a block. 
+# If a local variable is defined in a block, its scope includes 
+# that block. If the definition occurs in a function block, the 
+# scope extends to any blocks contained within the defining one, 
+# unless a contained block introduces a different binding for the 
+# name. 
+
+"""
+class Foo:
+    print 'making class Foo.'
+    x = 10
+    for i in xrange(10):
+        def goo(self):
+            print self.x
+            print self.__class__.x
+            print Foo.x
+            xxx = i
+            print xxx
+        class GOOF:
+            print 'trying to make GOOF class...'
+            xxx = i
+            print 'GOOF:', xxx
+            try:
+                print i
+            except NameError:
+                pass
+            print 'done making GOOD class'
+        #print 'GOOF.xxx:', GOOF.xxx
+
+        print 'i:', i, goo
+
+    print 'end making class Foo'
 
 
 try:
@@ -75,3 +103,5 @@ ROOF()
 ##
 ##
 ##goo2()()
+
+"""

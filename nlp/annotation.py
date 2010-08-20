b39f81a6e2079ffa479230e898288ef17801911d
timv@herman <none@none>
2010-08-20 12:49:28 -0400

    added a few more utlils to nlp/annotation

--- nlp/annotation.py
+++ nlp/annotation.py
@@ -1,6 +1,32 @@
 import re
 
+class ParseError(Exception):
+    """ Custom exception class used by this module. """
+    pass
+
+NEWLINE = '-NEWLINE-'
+
 def xml2segments(x):
+    """
+    Generate a segmentation from an xml-style annotation.
+    All the assumptions in `xml2bio` still apply.
+
+    >>> x = xml2segments("<title>Cat in the Hat</title><author>Dr. Seuss</author>")
+    >>> list(x)
+    [('title', ['Cat', 'in', 'the', 'Hat']), ('author', ['Dr.', 'Seuss'])]
+
+    """
+    x = re.sub('\n', ' %s ' % NEWLINE, x)
+    x = re.sub('(<[/]?[A-Za-z0-9]+>)', r' \1 ', x)
+    for label, tagged, close, word in re.findall('(?:(?:\s*<([A-Za-z0-9]+)>\s*([\w\W]+?)\s*</([A-Za-z0-9]+)>\s*)|([\w\W]+?)(?:\s+|$))', x):
+        if close != label:
+            raise ParseError('Mismatched xml tags (%s, %s)' % (close, label))
+        if word:
+            yield ('O', [word])
+        else:
+            yield (label, tagged.split())
+
+def xml2bio(x):
     """
     Generate BIO-token pairs from xml-style annotation.
     Notes:
@@ -9,17 +35,44 @@ def xml2segments(x):
       3) Assumes no nesting (tags within tags)
       4) Converts newlines into a token "-NEWLINE-"
 
-    >>> x = xml2segments("<title>Cat in the Hat</title><author>Dr. Seuss</author>")
-    >>> list(x)
-    [('title', ['Cat', 'in', 'the', 'Hat']), ('author', ['Dr.', 'Seuss'])]
-
+    >>> x = xml2bio("<title>Cat in the Hat</title><author>Dr. Seuss</author>")
+    >>> list(x)                                  #doctest:+NORMALIZE_WHITESPACE
+    [('B-title', 'Cat'), ('I-title', 'in'), ('I-title', 'the'), 
+     ('I-title', 'Hat'), ('B-author', 'Dr.'), ('I-author', 'Seuss')]
     """
-    x = re.sub('\n', ' -NEWLINE- ', x)
+    x = re.sub('\n', ' %s ' % NEWLINE, x)
     x = re.sub('(<[/]?[A-Za-z0-9]+>)', r' \1 ', x)
     for label, tagged, close, word in re.findall('(?:(?:\s*<([A-Za-z0-9]+)>\s*([\w\W]+?)\s*</([A-Za-z0-9]+)>\s*)|([\w\W]+?)(?:\s+|$))', x):
-        assert close == label, (close, label)
+        if close != label:
+            raise ParseError('Mismatched xml tags (%s, %s)' % (close, label))
         if word:
-            yield ('O', [word])
+            yield ('O', word)
         else:
-            yield (label, tagged.split())
+            words = iter(tagged.split())
+            yield ('B-%s' % label, words.next())
+            for w in words:
+                yield ('I-%s' % label, w)
+
+def bracket2bio(x):
+    """
+    generate BIO-token pairs from bracket-style annotation.
+    Note: splits text of spaces, so wordsplitting should already be done.
+
+    >>> x = bracket2bio("[TITLE Cat in the Hat][AUTHOR Dr. Seuss]")
+    >>> list(x)                                  #doctest:+NORMALIZE_WHITESPACE
+    [('B-TITLE', 'Cat'), ('I-TITLE', 'in'), ('I-TITLE', 'the'), 
+     ('I-TITLE', 'Hat'), ('B-AUTHOR', 'Dr.'), ('I-AUTHOR', 'Seuss')]
+    """
+    if '\n' in x:
+        raise ParseError('No newlines allowed in brack2bio annotation.')
+    for label, tagged, word in re.findall('(?:(?:\[([A-Z0-9]+)\s+(.+?)\s*\]\s*)|(.+?)(?:\s+|$))', x):
+        if word:
+            yield ('O', word)
+        else:
+            words = iter(tagged.split())
+            yield ('B-%s' % label, words.next())
+            for w in words:
+                if '[' in w or ']' in w:
+                    raise ParseError('brackets can not appear within a word.')
+                yield ('I-%s' % label, w)
 

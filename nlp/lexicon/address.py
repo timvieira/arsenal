81a9d5e6ed270593d7d091b0491778668a9502c5
timv@herman <none@none>
2010-08-28 19:27:02 -0400

    created nlp.lexicon.address. might want to add other things to it.. merge with other modules

---
+++ nlp/lexicon/address.py
@@ -0,0 +1,7 @@
+cardinal_direction = frozenset([
+    'north', 'south', 'east', 'west',
+    'northwest', 'southwest', 'northeast', 'southest',
+    'NW', 'SW', 'NE', 'SE'
+    'N.W.', 'S.W.', 'N.E.', 'S.E.'
+])
+

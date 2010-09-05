84f4712fcaf162671cfda7404256dc5459e5421a
timv@herman <none@none>
2010-09-05 00:44:24 -0400

    added terminal.misc a dumping place for terminal utilities. Right now all it has is a utility for figuring out the terminal width (works on Windows and Linux).

---
+++ terminal/misc.py
@@ -0,0 +1,57 @@
+import os, sys
+
+def console_width():
+    """Return width of available window area. Autodetection works for
+    Windows and POSIX platforms. Returns 80 for others
+
+    Code from http://bitbucket.org/techtonik/python-pager
+    """
+
+    if os.name == 'nt':
+        STD_INPUT_HANDLE  = -10
+        STD_OUTPUT_HANDLE = -11
+        STD_ERROR_HANDLE  = -12
+
+        # get console handle
+        from ctypes import windll, Structure, byref
+        from ctypes.wintypes import SHORT, WORD, DWORD
+        console_handle = windll.kernel32.GetStdHandle(STD_OUTPUT_HANDLE)
+
+        # CONSOLE_SCREEN_BUFFER_INFO Structure
+        class COORD(Structure):
+            _fields_ = [("X", SHORT), ("Y", SHORT)]
+
+        class SMALL_RECT(Structure):
+            _fields_ = [("Left", SHORT), ("Top", SHORT),
+                        ("Right", SHORT), ("Bottom", SHORT)]
+
+        class CONSOLE_SCREEN_BUFFER_INFO(Structure):
+            _fields_ = [("dwSize", COORD),
+                        ("dwCursorPosition", COORD),
+                        ("wAttributes", WORD),
+                        ("srWindow", SMALL_RECT),
+                        ("dwMaximumWindowSize", DWORD)]
+
+        sbi = CONSOLE_SCREEN_BUFFER_INFO()
+        ret = windll.kernel32.GetConsoleScreenBufferInfo(console_handle, byref(sbi))
+        if ret == 0:
+            return 0
+        return sbi.srWindow.Right+1
+
+    elif os.name == 'posix':
+        from fcntl import ioctl
+        from termios import TIOCGWINSZ
+        from array import array
+
+        winsize = array("H", [0] * 4)
+        try:
+            ioctl(sys.stdout.fileno(), TIOCGWINSZ, winsize)
+        except IOError:
+            pass
+        return (winsize[1], winsize[0])[0]
+
+    # the fall back value
+    return 80
+
+if __name__ == '__main__':
+    print console_width()

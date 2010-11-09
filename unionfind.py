3b6d1e789b2dc3d96eb9c2ff60b0bc6c7d96f5fd
timv@herman <none@none>
2010-11-09 13:43:30 -0800

    using a cleaner implemented of unionfind

--- unionfind.py
+++ unionfind.py
@@ -1,83 +1,64 @@
-class UnionFind:
-    def __init__(self):
-        ''' Create an empty union find data structure. '''
-        self.num_weights = {}
-        self.parent_pointers = {}
-        self.num_to_objects = {}
-        self.objects_to_num = {}
-        self.__repr__ = self.__str__
-    def insert_objects(self, objects):
-        ''' Insert a sequence of objects into the structure.  All must be Python hashable. '''
-        for obj in objects:
-            self.find(obj);
-    def find(self, object):
-        ''' Find the root of the set that an object is in.
-            If the object was not known, will make it known, and it becomes its own set.
-            Object must be Python hashable. '''
-        if not object in self.objects_to_num:
-            obj_num = len(self.objects_to_num)
-            self.num_weights[obj_num] = 1
-            self.objects_to_num[object] = obj_num
-            self.num_to_objects[obj_num] = object
-            self.parent_pointers[obj_num] = obj_num
-            return object
-        stk = [self.objects_to_num[object]]
-        par = self.parent_pointers[stk[-1]]
-        while par != stk[-1]:
-            stk.append(par)
-            par = self.parent_pointers[par]
-        for i in stk:
-            self.parent_pointers[i] = par
-        return self.num_to_objects[par]
-    def union(self, object1, object2):
-        ''' Combine the sets that contain the two objects given.
-            Both objects must be Python hashable.
-            If either or both objects are unknown, will make them known, and combine them. '''
-        o1p = self.find(object1)
-        o2p = self.find(object2)
-        if o1p != o2p:
-            on1 = self.objects_to_num[o1p]
-            on2 = self.objects_to_num[o2p]
-            w1 = self.num_weights[on1]
-            w2 = self.num_weights[on2]
-            if w1 < w2:
-                o1p, o2p, on1, on2, w1, w2 = o2p, o1p, on2, on1, w2, w1
-            self.num_weights[on1] = w1+w2
-            del self.num_weights[on2]
-            self.parent_pointers[on2] = on1
-    def __str__(self):
-        ''' Included for testing purposes only.
-            All information needed from the union find data structure can be attained using find. '''
-        sets = {}
-        for i in xrange(len(self.objects_to_num)):
-            sets[i] = []
-        for i in self.objects_to_num:
-            sets[self.objects_to_num[self.find(i)]].append(i)
-        out = []
-        for i in sets.itervalues():
-            if i:
-                out.append(repr(i))
-        return ', '.join(out)
-    def get_sets(self):
-        sets = {}
-        for i in xrange(len(self.objects_to_num)):
-            sets[i] = []
-        for i in self.objects_to_num:
-            sets[self.objects_to_num[self.find(i)]].append(i)
-        return sets.values()
+"""UnionFind.py
 
-if __name__ == '__main__':
-    print "Testing..."
-    uf = UnionFind()
-    az = "abcdefghijklmnopqrstuvwxyz"
-    az += az.upper()
-    uf.insert_objects(az)
-    import random
-    cnt = 0
-    while len(uf.num_weights) > 20:
-        cnt += 1
-        uf.union(random.choice(az), random.choice(az))
-    print uf, cnt
-    print "Testing complete."
+Union-find data structure. Based on Josiah Carlson's code,
+http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/215912
+with significant additional changes by D. Eppstein.
+"""
+
+class UnionFind(object):
+    """Union-find data structure.
+
+    Each unionFind instance X maintains a family of disjoint sets of
+    hashable objects, supporting the following two methods:
+
+    - X[item] returns a name for the set containing the given item.
+      Each set is named by an arbitrarily-chosen one of its members; as
+      long as the set remains unchanged it will keep the same name. If
+      the item is not yet part of a set in X, a new singleton set is
+      created for it.
+
+    - X.union(item1, item2, ...) merges the sets containing each item
+      into a single larger set.  If any item is not yet part of a set
+      in X, it is added to X as one of the members of the merged set.
+    """
+
+    def __init__(self):
+        """Create a new empty union-find structure."""
+        self.weights = {}
+        self.parents = {}
+
+    def __getitem__(self, obj):
+        """Find and return the name of the set containing the object."""
+
+        # check for previously unknown object
+        if obj not in self.parents:
+            self.parents[obj] = obj
+            self.weights[obj] = 1
+            return obj
+
+        # find path of objects leading to the root
+        path = [obj]
+        root = self.parents[obj]
+        while root != path[-1]:
+            path.append(root)
+            root = self.parents[root]
+
+        # compress the path and return
+        for ancestor in path:
+            self.parents[ancestor] = root
+        return root
+        
+    def __iter__(self):
+        """Iterate through all items ever found or unioned by this structure."""
+        return iter(self.parents)
+
+    def union(self, *objects):
+        """Find the sets containing the objects and merge them all."""
+        roots = [self[x] for x in objects]
+        heaviest = max([(self.weights[r],r) for r in roots])[1]
+        for r in roots:
+            if r != heaviest:
+                self.weights[heaviest] += self.weights[r]
+                self.parents[r] = heaviest
 
 

ef57a621692288d221ea2ecc42d8939e0023456c
timv@herman <none@none>
2010-12-06 16:01:43 -0500

    improvements to gcal quickadd

--- gcal/quickadd.py
+++ gcal/quickadd.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python
 
-import os, sys, getpass
+import sys, webbrowser
+from getpass import getpass
 
-from gcal import atom, gdata
-import gdata.calendar
-import gdata.calendar.service
+from gcal.atom import Content
+from gdata.calendar import CalendarEventEntry, QuickAdd
+from gdata.calendar.service import CalendarService
 
-email = 'timsfanmail'
-password = getpass.getpass()
+def quickadd(email, content, open_url=True):
 
-cal_client = gdata.calendar.service.CalendarService(email, password, 'Google-Calendar_Python_Sample-1.0')
-cal_client.ProgrammaticLogin()
+    client = CalendarService(email, getpass(), 'Google-Calendar_Python_Sample-1.0')
+    client.ProgrammaticLogin()
 
-def quickadd(content):
-    event = gdata.calendar.CalendarEventEntry()
-    event.content   = atom.Content(text=content)
-    event.quick_add = gdata.calendar.QuickAdd(value='true')
-    new_event = cal_client.InsertEvent(event, '/calendar/feeds/default/private/full')
+    event = CalendarEventEntry()
+    event.content = Content(text=content)
+    event.quick_add = QuickAdd(value='true')
+    new_event = client.InsertEvent(event, '/calendar/feeds/default/private/full')
 
-quickadd(' '.join(sys.argv[1:]))
-os.system('firefox http://google.com/calendar 2>/dev/null &')
+    print 'created event: %r' % new_event.title.text
+    if open_url:
+        webbrowser.open(new_event.link[0].href)
+
+if __name__ == '__main__':
+    quickadd('timsfanmail', ' '.join(sys.argv[1:]))

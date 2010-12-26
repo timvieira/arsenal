e8f0b866e6ba6bc951181e6bc674590ea787f528
timv <tim.f.vieira@gmail.com>
2010-12-25 21:37:30 -0500

    * removed dependence on `flipdict` or `numpy` * replaced `get_intarray` method with `map` * no more `InvalidKeyException` using `ValueError` instead. * when an Alphabet which is no longer growing is frozen you shouldn't be able to add keys.

--- alphabet.py
+++ alphabet.py
@@ -1,18 +1,39 @@
-from flipdict import Flipdict
 
-# TODO:
-#  * use an array for the int->str mapping and dict for str->int
+
 class Alphabet(object):
-    """ Bijective mapping from strings to integers. """
+    """
+    Bijective mapping from strings to integers.
+
+    >>> a = Alphabet()
+    >>> [a[x] for x in 'abcdefg']
+    [0, 1, 2, 3, 4, 5, 6]
+    >>> map(a.lookup, range(7))
+    ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+
+    >>> a.stop_growth()
+    >>> a['A']
+
+    >>> a.freeze()
+    >>> a.add('z')
+    Traceback (most recent call last):
+      ...
+    ValueError: Alphabet is frozen. Key "z" not found.
+
+    """
+
     def __init__(self):
-        self.mapping = Flipdict()
+        self.mapping = {}
+        self.flip = {}
         self.i = 0
         self.frozen = False
         self.growing = True
+
     def freeze(self):
         self.frozen = True
+
     def stop_growth(self):
         self.growing = False
+
     @classmethod
     def from_iterable(cls, it):
         inst = cls()
@@ -20,30 +41,45 @@ class Alphabet(object):
             inst.add(x)
         inst.freeze()
         return inst
+
     def lookup(self, i):
         assert isinstance(i, int)
-        return self.mapping.flip[i]
+        return self.flip[i]
+
+    def map(self, seq):
+        """ apply alphabet to sequence """
+        for s in seq:
+            x = self[s]
+            if x is not None:
+                yield x
+
     def __getitem__(self, k):
         try:
             return self.mapping[k]
         except KeyError:
             if not isinstance(k, basestring):
-                raise InvalidKeyException("Invalid key (%s): only strings allowed." % k)
+                raise ValueError("Invalid key (%s): only strings allowed." % k)
+            if self.frozen:
+                raise ValueError('Alphabet is frozen. Key "%s" not found.' % k)
             if not self.growing:
                 return None
-            if self.frozen:
-                raise AssertionError('Alphabet is frozen and key (%s) was not found.' % k)
-            self.mapping[k] = self.i
+            x = self.mapping[k] = self.i
+            self.flip[x] = k
             self.i += 1
-            return self.mapping[k]
+            return x
+
     add = __getitem__
+
     def __iter__(self):
         return iter(self.mapping)
+
     def __len__(self):
         return len(self.mapping)
+
     def plaintext(self):
         return '\n'.join(self.lookup(i) for i in xrange(self.i))
 
 
-class InvalidKeyException(Exception):
-    pass
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()

4a77684b176df004815365d55e5131bdd5a201eb
timv <tim.f.vieira@gmail.com>
2011-02-09 18:16:45 -0500

    added force_Unicode to string_utils.

--- string_utils.py
+++ string_utils.py
@@ -2,6 +2,52 @@
 
 import re
 
+
+
+# Borrowed from: http://www.codigomanso.com/en/2010/05/una-de-python-force_unicode/
+def force_unicode(s, encoding='utf-8', errors='ignore'):
+    """
+    Returns a unicode object representing 's'. Treats bytestrings using the
+    'encoding' codec.
+    """
+    import codecs
+    if s is None:
+        return u''
+    try:
+        if not isinstance(s, basestring,):
+            if hasattr(s, '__unicode__'):
+                s = unicode(s)
+            else:
+                try:
+                    s = unicode(str(s), encoding, errors)
+                except UnicodeEncodeError:
+                    if not isinstance(s, Exception):
+                        raise
+                    # If we get to here, the caller has passed in an Exception
+                    # subclass populated with non-ASCII data without special
+                    # handling to display as a string. We need to handle this
+                    # without raising a further exception. We do an
+                    # approximation to what the Exception's standard str()
+                    # output should be.
+                    s = ' '.join(force_unicode(arg, encoding, errors) for arg in s)
+        elif not isinstance(s, unicode):
+            # Note: We use .decode() here, instead of unicode(s, encoding,
+            # errors), so that if s is a SafeString, it ends up being a
+            # SafeUnicode at the end.
+            s = s.decode(encoding, errors)
+    except UnicodeDecodeError, e:
+        if not isinstance(s, Exception):
+            raise UnicodeDecodeError (s, *e.args)
+        else:
+            # If we get to here, the caller has passed in an Exception
+            # subclass populated with non-ASCII bytestring data without a
+            # working unicode method. Try to handle this without raising a
+            # further exception by individually forcing the exception args
+            # to unicode.
+            s = ' '.join(force_unicode(arg, encoding, errors) for arg in s)
+    return unicode(s)
+
+
 def PRE(x):
     return '<pre>' +  x.replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;') + '</pre>'
 

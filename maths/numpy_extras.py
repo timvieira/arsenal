7edec4e39d5d69da7be796e94c89cb0f680c2966
timv <tim.f.vieira@gmail.com>
2011-02-16 15:12:58 -0500

    added mutual information.

--- maths/numpy_extras.py
+++ maths/numpy_extras.py
@@ -1,4 +1,4 @@
-from numpy.core import array, log, dot, abs as np_abs
+from numpy.core import array, exp, log, dot, abs as np_abs, multiply
 
 def logsumexp(a):
     """
@@ -20,7 +20,7 @@ def kl_divergence(p, q):
     NOTE: If any value in q is 0.0 then the KL-divergence is infinite.
     """
     assert len(p) == len(q)
-    p = p[p.nonzero()]
+    p = p[p > 0]
     return dot(p, log(p / q)) / log_of_2
 
 # KL(p||q) = sum_i p[i] log(p[i] / q[i])
@@ -28,8 +28,36 @@ def kl_divergence(p, q):
 #          = sum_i p[i] log p[i] - sum_i p[i] log(q[i])
 #          = Entropy(p) + CrossEntropy(p,q)
 
+# timv: do we need to specify all three things?
+# 
+#  P(x) = sum_y P(x,y) by law of total probabilty
+def mutual_information(joint):
+    """
+    Mutual Information
+
+    MI(x,y) = KL( p(x,y) || p(x) p(y) )
+
+    joint = p(x,y)
+    p = p(x)
+    q = q(y)
+
+    relationships: 
+      MI(x,y) is the expected PMI(x,y) wrt p(x,y)
+      MI(x,y) = KL( p(x,y) || p(x) p(y) )
+
+    properties:
+      MI(X,Y) = MI(Y,X) is symmetric
+    """
+    # we can compute px and py from joint by applying the law of total probability
+    px = joint.sum(axis=1)
+    py = joint.sum(axis=0)
+    independent = multiply.outer(px, py)
+    assert joint.shape == independent.shape
+    return kl_divergence(array(joint.flat), array(independent.flat))
+
+
 def cross_entropy(p, q):
-    """ Cross Entropy of two vectors, 
+    """ Cross Entropy of two vectors,
 
     CE(p,q) = - \sum_i p[i] log q[i]
 
@@ -53,28 +81,54 @@ def normalize(p):
 def lidstone(p, delta):
     return normalize(p + delta)
 
-def equal(a, b, epsilon=1e-10):
-    "L_{\inf}(a - b) < epsilon"
-    return np_abs(a - b).max() < epsilon
+def equal(a, b, tol=1e-10):
+    "L_{\inf}(a - b) < tol"
+    return inf_norm(a,b) < tol
+
+def inf_norm(a, b):
+    return np_abs(a - b).max()
+
+def assert_equal(a, b, tol=1e-10):
+    err = inf_norm(a,b)
+    assert err < tol, 'error = %s >= tolerance (%s)' % (err, tol)
 
 if __name__ == '__main__':
-    import doctest; doctest.testmod()
+    #import doctest; doctest.testmod()
 
     def tests():
+
         # Entropy tests
         assert entropy(array((0.5, 0.5))) == 1.0
         assert abs(entropy(array((0.75, 0.25))) - 0.8112781244) < 1e-10
         assert abs(entropy(array((0.1, 0.1, 0.8))) - 0.9219280948) < 1e-10
-    
+
         # KL-divergence tests
         assert kl_divergence(array((0.5, 0.5)), array((0.5, 0.5))) == 0.0
 
         # KL, Entropy, and Cross Entropy relationship
         p = array([0.5, 0.5])
         q = array([0.4, 0.6])
-        print equal(cross_entropy(p, q), (entropy(p) + kl_divergence(p, q)))
+        assert_equal(cross_entropy(p, q), (entropy(p) + kl_divergence(p, q)))
 
         # Normalize tests
-        assert equal(array([0.5, 0.5]), normalize(array([2, 2])))
+        assert_equal(array([0.5, 0.5]), normalize(array([2, 2])))
+
+        # Mutual Information tests
+        #Pjoint = array([[0.25, 0.25], [0.25, 0.25]])
+ 
+        import numpy as np
+        Pjoint = normalize(np.random.rand(4,10))
+        assert_equal(mutual_information(Pjoint),
+                     mutual_information(Pjoint.T))
+
+        def mi_independent_is_zero(px, py):
+            assert_equal(mutual_information(multiply.outer(px, py)), 0.0)
+
+        mi_independent_is_zero(p, q)
+        mi_independent_is_zero(array([0.1, 0.1, 0.2, 0.6]),   # non-square joint
+                               array([0.9, 0.1]))
+
+
+        print 'passed tests.'
 
     tests()

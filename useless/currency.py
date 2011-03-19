1ab7e8fe81d88f6dd856ab88019873d65bd3f104
timv <tim.f.vieira@gmail.com>
2011-03-19 14:48:54 -0400

    updated currency conversion program.

--- useless/currency.py
+++ useless/currency.py
@@ -1,54 +1,26 @@
-#!/usr/bin/python
-from urllib2 import urlopen
-import re, sys
+#!/usr/bin/env python
 
-#from cache.memoize import memoize_persistent
-#from functools import partial
-#@partial(memoize_persistent, filename='urlcache.pkl')
+import re
+import sys
+from urllib2 import urlopen
+
 def readurl(url):
     return urlopen(url).read()
 
-
-def oanda(fromcurr, tocurr):
-    # http://www.oanda.com/currency/historical-rates?expr2=EUR&exch2=USD&format=ASCII&date1=01/13/11&date=01/19/11&lang=en&result=1 
-
-    fromdate = '01/13/11'
-    todate = '01/19/11'
-
-    url = 'http://www.oanda.com/currency/historical-rates?lang=en&result=1&format=ASCII'
-    url += '&exch2=%s&expr2=%s' % (fromcurr, tocurr)
-    url += '&date1=%s&date=%s' % (fromdate, todate)
-
-    #content = urlopen(url).read()
-    content = readurl(url)
-
-    pre = re.findall('<pre.*>([\w\W]*?)</pre>', content, re.IGNORECASE)
-    assert len(pre) == 1
-    print [(a, float(b)) for (a,b) in (x.split() for x in pre[0].strip().split('\n'))]
-
-    
 def google(currencyFrom, currencyTo):
     assert currencyFrom != currencyTo
-
     url = 'http://www.google.com/ig/calculator?hl=en&q=1' + currencyFrom + '%3D%3F' + currencyTo
-    content = readopen(url)
-
+    content = readurl(url)
     try:
         [result] = re.findall(".*rhs:\s*\"(\d\.\d*)", content)
-        result = float(result)
-        print currencyFrom + " converted to " + currencyTo + ": %s" % result
-
-    except:
+        print currencyFrom + " converted to " + currencyTo + ": %s" % float(result)
+    except ValueError:
         print 'ERROR:', content
 
- 
-def test():
-    sourceCurrencyList = ['USD','EUR']
-    targetCurrencyList = ['EUR', 'GBP']
-
-    for source in sourceCurrencyList:
-        for target in targetCurrencyList:
-            if source != target:
-                google(source,target)
- 
-
+if __name__ == '__main__':
+    try:
+        f, t = sys.argv[1:]
+    except ValueError:
+        print 'usage: %s <from> <to>' % sys.argv[0]
+        sys.exit(1)
+    google(f,t)

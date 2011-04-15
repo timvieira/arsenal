a40674f6ba1da74c2f4d286eceaa59ca32ea86be
timv <tim.f.vieira@gmail.com>
2011-04-15 19:40:33 -0400

    remove python-snippets from projects.

--- python-snippets/module_inspection.py
+++
@@ -1,9 +0,0 @@
-
-import pyclbr   # parse a python module and describe its classes and methods
-import pprint
-
-misc_clbr = pyclbr.readmodule('lib.misc')
-pprint.pprint(misc_clbr)
-
-misc_clbr = pyclbr.readmodule('lib.nlp.wordnet.cache')
-pprint.pprint(misc_clbr)
--- python-snippets/swigtest/example.h
+++
@@ -1,4 +0,0 @@
-/* File: example.h */
-
-int fact(int n);
-

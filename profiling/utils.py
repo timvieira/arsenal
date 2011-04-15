48ba2c91a4ee70cb51b5e4996e37a726db984c00
timv <tim.f.vieira@gmail.com>
2011-04-15 19:42:31 -0400

    cleanup

--- profiling/utils.py
+++ profiling/utils.py
@@ -4,6 +4,7 @@ import os
 import sys
 import pstats
 import cProfile
+import tempfile
 
 # TODO: 
 #  - maybe we should delete the `out` tempfile
@@ -39,32 +40,11 @@ def kcachegrind(cmd, out='profile.kgrind'):
     os.system("kcachegrind %s &" % out)
 
 
-## from cStringIO import StringIO
-## import time, hotshot, hotshot.stats, tempfile
-## def profile(f, *args, **kw):
-##     """
-##     Profiles function `f` and returns a tuple containing its output
-##     and a string with human-readable profiling information.
-##     """
-##     temp = tempfile.NamedTemporaryFile()
-##     prof = hotshot.Profile(temp.name)
-##
-##     stime = time.time()
-##     result = prof.runcall(f, *args, **kw)
-##     stime = time.time() - stime
-##     prof.close()
-##
-##     out = StringIO()
-##     stats = hotshot.stats.load(temp.name)
-##     stats.stream = out
-##     stats.strip_dirs()
-##     stats.sort_stats('time', 'calls')
-##     stats.print_stats()
-##     stats.print_callers()
-##
-##     x =  '\n\ntook '+ str(stime) + ' seconds\n'
-##     x += out.getvalue()
-##
-##     return (result, x)
-
-
+if __name__ == '__main__':
+    viz = profile_viz
+    if '--kgrind' in sys.argv:
+        viz = kcachegrind
+        sys.argv.remove('--kgrind')  # hack sys.argv
+    if len(sys.argv) > 1:
+        sys.argv.pop(0)  # hack sys.argv
+        viz('execfile(%r)' % sys.argv[0])

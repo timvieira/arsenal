cc42471aa0a1d359fb4dce8acac70c233091df58
timv <tim.f.vieira@gmail.com>
2011-07-15 19:36:05 -0400

    ()

--- web/download.py
+++ web/download.py
@@ -1,13 +1,21 @@
-import os
+import re, os
 from urllib2 import urlopen
 from fsutils import ensure_dir
+#from urlparse import urlparse
 
 def download(url, usecached=True, cachedir='cache/'):
     ensure_dir(cachedir)
-    cached = cachedir + '/' + url.replace('/','#')
+
+    # TODO: handle ftp, http, etc
+    #if urlparse(url).scheme.startswith('http')
+
+    ensure_dir(cachedir)
+
+    cached = cachedir + '/' + url.replace('/','#')   # TODO: store urls like wget
     if usecached and os.path.exists(cached):
         with file(cached) as f:
             return f.read()
+
     try:
         with file(cached, 'wb') as f:
             x = urlopen(url).read()

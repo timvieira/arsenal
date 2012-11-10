d294f9ddd6759264b8ab0c106551eff9efa83904
timv <tim.f.vieira@gmail.com>
2012-11-09 23:03:01 -0500

    util to grab password from keyring.

--- misc.py
+++ misc.py
@@ -10,6 +10,28 @@ from threading import Thread
 from terminal import colors
 
 
+import keyring
+from getpass import getpass
+
+def set_password(service, user, pw=None):
+    print 'Set password:'
+    if pw is None:
+        pw = getpass()
+        print 'Type password again (for verification):'
+        if pw != getpass():
+            print 'Error: passwords did not match. Try again.'
+            set_password(service, user)
+    keyring.set_password(service, user, pw)
+    return pw
+
+
+def password(service, user):
+    pw = keyring.get_password(service, user)
+    if pw is None:
+        pw = set_password(service, user)
+    return pw
+
+
 @contextmanager
 def ignore_error(color='red'):
     try:

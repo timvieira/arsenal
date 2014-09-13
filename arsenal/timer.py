d711cea04939267ec2951568920e49435b3b09f7
timv <tim.f.vieira@gmail.com>
2014-09-13 10:39:11 -0400

    added additional options to Timer.compare and Timer.compare_many. It's easier to compare different attributes (e.g., total time instead of average time).

--- arsenal/timer.py
+++ arsenal/timer.py
@@ -2,6 +2,8 @@ import numpy as np
 from time import time
 from contextlib import contextmanager
 from arsenal.humanreadable import htime
+from arsenal.terminal import yellow
+
 
 class Timer(object):
     """
@@ -20,30 +22,49 @@ class Timer(object):
         self.name = name
         self.times = []
         self.b4 = None
+
     def __enter__(self):
         self.b4 = time()
+
     def __exit__(self, *_):
         self.times.append(time() - self.b4)
+
     def __str__(self):
         return 'Timer(name=%s, avg=%g, std=%g)' % (self.name, self.avg, self.std)
+
     @property
     def avg(self):
         return np.mean(self.times)
+
     @property
     def std(self):
         return np.std(self.times, ddof=1)
-    def compare(self, other):
+
+    @property
+    def total(self):
+        return sum(self.times)
+
+    def compare(self, other, attr='avg', verbose=True):
         if len(self.times) == 0 or len(other.times) == 0:
             print '%s %s %s' % (self.name, '???', other.name)
             return
-        if self.avg <= other.avg:
-            print '%s is %6.4fx faster than %s' % (self.name, other.avg / self.avg, other.name)
+        if getattr(self, attr) <= getattr(other, attr):
+            print '%s is %6.4fx faster than %s %s' \
+                % (self.name,
+                   getattr(other, attr) / getattr(self, attr),
+                   other.name,
+                   ((yellow % '(%s: %s: %g %s: %g)' % (attr,
+                                                      other.name, getattr(other, attr),
+                                                      self.name, getattr(self, attr)))
+                    if verbose else '')
+                )
         else:
-            other.compare(self)
-    def compare_many(self, *others):
+            other.compare(self, attr=attr, verbose=verbose)
+
+    def compare_many(self, *others, **kw):
         #self.compare(self)
         for x in others:
-            self.compare(x)
+            self.compare(x, **kw)
 
     def plot(self, *others):
         import pylab as pl

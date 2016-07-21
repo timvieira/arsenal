8e5c3a7092e3fcb3dd0d7d11734730021ad7e977
timv <tim.f.vieira@gmail.com>
2016-07-21 15:25:22 -0400

    added stuff to __init__ so that import-paths to stuff are shorter

---
+++ arsenal/__init__.py
@@ -0,0 +1,9 @@
+from arsenal.iterview import iterview
+from arsenal.terminal import colors
+from arsenal.alphabet import Alphabet
+from arsenal.timer import Timer, timers, timeit
+from arsenal.viz import axman, update_ax
+from arsenal import math
+from arsenal.debug import ip
+#from arsenal.humanreadable import htime
+from arsenal.misc import ddict

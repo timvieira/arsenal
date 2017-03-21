8977556e63b5b9b53af786215e9bf70a4f180cd8
timv <tim.f.vieira@gmail.com>
2017-03-21 10:09:17 -0400

    minor cleanups in combinatorics...

--- arsenal/math/combinatorics.py
+++ arsenal/math/combinatorics.py
@@ -1,13 +1,13 @@
 def k_subsets_i(n, k):
-    """ Yield each subset of size k from the set of integers 0 .. n - 1 """
+    "Subset of size k from the set of integers 0..n-1"
     # check base cases
     if k == 0 or n < k:
         yield []
     elif n == k:
-        yield range(n) 
+        yield range(n)
     else:
         # Use recursive formula based on binomial coeffecients:
-        # choose(n, k) = choose(n - 1, k - 1) + choose(n - 1, k)
+        # choose(n, k) = choose(n-1, k-1) + choose(n-1, k)
         for s in k_subsets_i(n - 1, k - 1):
             s.append(n - 1)
             yield s
@@ -15,27 +15,20 @@ def k_subsets_i(n, k):
             yield s
 
 def k_subsets(s, k):
-    """ Yield all subsets of size k from s """
+    "Subsets of size k from s."
     s = list(s)
     n = len(s)
     for k_set in k_subsets_i(n, k):
         yield [s[i] for i in k_set]
 
 def powerset(S):
-    # number of variables
     n = len(S)
     # choose all subsets of size k from variables
-    for k in range(0,n+1):
+    for k in range(n+1):
         for s in k_subsets(S, k):
             yield list(s)
 
 # __________________________________________________________________________
-"""
-xpermutations.py
-Generators for calculating
-a) the permutations of a sequence and
-b) the combinations and selections of a number of elements from a sequence.
-"""
 
 def xcombinations(items, n):
     if n==0: yield []
@@ -50,7 +43,7 @@ def xuniqueCombinations(items, n):
         for i in xrange(len(items)):
             for cc in xuniqueCombinations(items[i+1:],n-1):
                 yield [items[i]]+cc
-            
+
 def xselections(items, n):
     if n==0: yield []
     else:
@@ -61,21 +54,32 @@ def xselections(items, n):
 def xpermutations(items):
     return xcombinations(items, len(items))
 
-if __name__=="__main__":
-    print "Permutations of 'love'"
-    for p in xpermutations(['l','o','v','e']): print ''.join(p)
+permutations = xpermutations
 
-    print
-    print "Combinations of 2 letters from 'love'"
-    for c in xcombinations(['l','o','v','e'],2): print ''.join(c)
 
-    print
-    print "Unique Combinations of 2 letters from 'love'"
-    for uc in xuniqueCombinations(['l','o','v','e'],2): print ''.join(uc)
+if __name__ == '__main__':
 
-    print
-    print "Selections of 2 letters from 'love'"
-    for s in xselections(['l','o','v','e'],2): print ''.join(s)
+    def test():
+        print "Permutations of 'love'"
+        for p in xpermutations(['l','o','v','e']):
+            print ''.join(p)
 
-    print
-    print map(''.join, list(xpermutations('done')))
+        print
+        print "Combinations of 2 letters from 'love'"
+        for c in xcombinations(['l','o','v','e'],2):
+            print ''.join(c)
+
+        print
+        print "Unique Combinations of 2 letters from 'love'"
+        for uc in xuniqueCombinations(['l','o','v','e'],2):
+            print ''.join(uc)
+
+        print
+        print "Selections of 2 letters from 'love'"
+        for s in xselections(['l','o','v','e'],2):
+            print ''.join(s)
+
+        print
+        print map(''.join, list(xpermutations('done')))
+
+    test()

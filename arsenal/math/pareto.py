f4065384f67d5a9c79cda5d25d1cb7e360b75a5e
timv <tim.f.vieira@gmail.com>
2017-04-12 01:12:03 -0400

    overhaul pareto front code -- now handling lots of special cases...

--- arsenal/math/pareto.py
+++ arsenal/math/pareto.py
@@ -7,7 +7,7 @@ from arsenal.terminal import yellow
 from arsenal.iterextras import window
 
 
-def pareto_frontier(X, Y, maxX=True, maxY=True):
+def pareto_frontier(X, Y, maxX=True, maxY=True, indices=False):
     """Determine Pareto frontier, returns list of sorted points.
 
     Args:
@@ -21,64 +21,58 @@ def pareto_frontier(X, Y, maxX=True, maxY=True):
     assert len(X) == len(Y)
     if len(X) == 0:
         return []
-    a = sorted(zip(X, Y), reverse=maxX)
+
+    xx = -1 if maxX else +1
+    yy = -1 if maxY else +1
+    key = lambda xy: (xy[0] * xx, xy[1] * yy)   # need to break ties
+
+    a = sorted(zip(X, Y, range(len(X))), key=key)
     frontier = []
-    lastx = float('-inf') if maxX else float('+inf')
-    lasty = float('-inf') if maxY else float('+inf')
+    lastx = float('inf') * xx
+    lasty = float('inf') * yy
     for xy in a:
-        x,y = xy
-        if maxY:
-            if y >= lasty:
-                if lastx != x:
-                    frontier.append(xy)
-                lasty = y
-                lastx = x
-        else:
-            if y <= lasty:
-                if lastx != x:
-                    frontier.append(xy)
-                lasty = y
-                lastx = x
-    return frontier
+        x,y,i = xy
+        if yy*y <= yy*lasty:
+            if lastx != x:
+                frontier.append(xy)
+            lasty = y
+            lastx = x
+
+    x,y,i = zip(*frontier)
+    return list(i) if indices else zip(x,y)
 
 
-def pareto_ix(X, Y, maxX=True, maxY=True):
-    """Determine Pareto frontier, returns list of indices
-
-    Args:
-
-      X, Y: data.
-
-      maxX, maxY: (bool) whether to maximize or minimize along respective
-        coordinate.
-
-    """
-    assert len(X) == len(Y)
-    if len(X) == 0:
-        return []
-    a = sorted(zip(X, Y, range(len(X))), reverse=maxX)
-    frontier = []
-    lastx = float('-inf') if maxX else float('+inf')
-    lasty = float('-inf') if maxY else float('+inf')
-    for xyi in a:
-        x,y,i = xyi
-        if maxY:
-            if y >= lasty:
-                if lastx != x:
-                    frontier.append(i)
-                lasty = y
-                lastx = x
-        else:
-            if y <= lasty:
-                if lastx != x:
-                    frontier.append(i)
-                lasty = y
-                lastx = x
-    return frontier
+def pareto_ix(X, Y, *a, **kw):
+    "Determine Pareto frontier, returns list of indices"
+    return pareto_frontier(X, Y, *a, **kw)
+#    assert len(X) == len(Y)
+#    if len(X) == 0:
+#        return []
+#    a = sorted(zip(X, Y, range(len(X))), reverse=maxX)
+#    frontier = []
+#    lastx = float('-inf') if maxX else float('+inf')
+#    lasty = float('-inf') if maxY else float('+inf')
+#    for xyi in a:
+#        x,y,i = xyi
+#        if maxY:
+#            if y >= lasty:
+#                if lastx != x:
+#                    frontier.append(i)
+#                lasty = y
+#                lastx = x
+#        else:
+#            if y <= lasty:
+#                if lastx != x:
+#                    frontier.append(i)
+#                lasty = y
+#                lastx = x
+#    return frontier
 
 
 def show_frontier(X, Y, maxX=False, maxY=True, dots=False,
-                  XMAX=None, YMIN=None, ax=None, label=None,
+                  XMIN=None, XMAX=None,
+                  YMIN=None, YMAX=None,
+                  ax=None, label=None,
                   interpolation='pessimistic', **style):
     """Plot Pareto frontier.
 
@@ -118,7 +112,7 @@ def show_frontier(X, Y, maxX=False, maxY=True, dots=False,
         X = X[hull.vertices]
         Y = Y[hull.vertices]
 
-    assert not maxX and maxY, 'need to update some hardcoded logic'
+#    assert not maxX and maxY, 'need to update some hardcoded logic'
 
     f = pareto_frontier(X, Y, maxX=maxX, maxY=maxY)
     if not f:
@@ -128,16 +122,29 @@ def show_frontier(X, Y, maxX=False, maxY=True, dots=False,
         xs, ys = zip(*f)
         ax.scatter(xs, ys, lw=0, alpha=0.5, c=sty['c'])
 
+    XMIN = XMIN if XMIN is not None else min(X)
     XMAX = XMAX if XMAX is not None else max(X)
     YMIN = YMIN if YMIN is not None else min(Y)
-    if max(X) > XMAX:
-        print '[pareto] WARNING: max(X) > XMAX. Plot will not show these points.'
-    if min(Y) < YMIN:
-        print '[pareto] WARNING: min(X) < XIN. Plot will not show these points.'
+    YMAX = YMAX if YMAX is not None else max(Y)
+
+#    if max(X) > XMAX:
+#        print '[pareto] WARNING: max(X) > XMAX. Plot will not show these points.'
+#    if min(Y) < YMIN:
+#        print '[pareto] WARNING: min(X) < XIN. Plot will not show these points.'
 
     # Connect corners of frontier. The first and last points on frontier have
     # lines which surround the point cloud.
-    f = [(min(X), YMIN)] + f + [(XMAX, max(Y))]
+
+
+    # TODO: make this look nicer...
+    if maxX and maxY:
+        f = [(XMAX, YMIN)] + f + [(XMIN, YMAX)]
+    elif not maxX and maxY:
+        f = [(XMIN, YMIN)] + f + [(XMAX, YMAX)]
+    elif maxX and not maxY:
+        f = [(XMAX, YMAX)] + f + [(XMIN, YMIN)]
+    else:
+        f = [(XMIN, YMAX)] + f + [(XMAX, YMIN)]
 
     if interpolation == 'pessimistic':
         # Make line segments from adjacent points
@@ -202,29 +209,40 @@ class Pareto(object):
 
 
 def test():
-    from pandas import DataFrame
-    X = np.linspace(0.01, 1.0, 10)
-    Y = np.log(X)
-    Y -= Y.min()
-    Y /= Y.max()
-    Y *= 0.95
+    if 0:
+        from pandas import DataFrame
+        X = np.linspace(0.01, 1.0, 10)
+        Y = np.log(X)
+        Y -= Y.min()
+        Y /= Y.max()
+        Y *= 0.95
 
-    df = DataFrame({'X': X, 'Y': Y})
-    P = Pareto(df, 'X', 'Y')
+        df = DataFrame({'X': X, 'Y': Y})
+        P = Pareto(df, 'X', 'Y')
 
-    data = []
-    for val in np.linspace(0,1,15):
-        data.append(dict(val=val, x=P.lookup_x(val), y=P.lookup_y(val)))
-        pl.axvline(val, alpha=.5)
-        pl.axhline(val, alpha=.5)
-    dd = DataFrame(data)
-    pl.scatter(dd.y, dd.val, lw=0, c='r')
-    pl.scatter(dd.val, dd.x, lw=0, c='g')
-    print dd
+        data = []
+        for val in np.linspace(0,1,15):
+            data.append(dict(val=val, x=P.lookup_x(val), y=P.lookup_y(val)))
+            pl.axvline(val, alpha=.5)
+            pl.axhline(val, alpha=.5)
+        dd = DataFrame(data)
+        pl.scatter(dd.y, dd.val, lw=0, c='r')
+        pl.scatter(dd.val, dd.x, lw=0, c='g')
+        print dd
 
-    #P.scatter(c='r', lw=0)
-    P.show_frontier(c='r', lw=4)
-    pl.show()
+        #P.scatter(c='r', lw=0)
+        P.show_frontier(c='r', lw=4)
+        pl.show()
+
+    X,Y = np.random.normal(0,1,size=(2, 30))
+
+    for maxX in [0,1]:
+        for maxY in [0,1]:
+            pl.figure()
+            pl.title('max x: %s, max y: %s' % (maxX, maxY))
+            pl.scatter(X,Y,lw=0)
+            show_frontier(X, Y, maxX=maxX, maxY=maxY)
+            pl.show()
 
 
 if __name__ == '__main__':

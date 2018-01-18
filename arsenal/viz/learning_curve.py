3b84e8fc7a0d418254589693aa4fcdce0612f0b3
timv <tim.f.vieira@gmail.com>
2018-01-17 19:50:39 -0500

    using order statistics instead of exponentially weighted moving averages for learning curve.

--- arsenal/viz/learning_curve.py
+++ arsenal/viz/learning_curve.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pylab as pl
 import pandas
 from collections import defaultdict
@@ -34,7 +35,7 @@ class LearningCurve(object):
                 ax.axhline(v, label=k, **sty[k])
             data = self.data
             for k, v in data.iteritems():
-                xs, ys = zip(*data[k])
+                xs, ys = np.array(data[k]).T
 
                 if self.averaging:
                     sty[k]['alpha'] = 0.5
@@ -48,12 +49,26 @@ class LearningCurve(object):
                     ax.scatter(xs, ys, label=k, **s)
 
                 if self.averaging:
-                    # TODO: averaging doesn't support irregular time steps.
-                    halflife = 10
-                    _,yy = zip(*data[k])
-                    ax.plot(xs,
-                            pandas.Series(yy).ewm(halflife=halflife).mean(),
-                            lw=2, c=l.get_color())
+                    s = pandas.Series(ys)
+
+                    if 0:
+                        halflife = 20
+                        r = s.ewm(halflife=halflife)
+                        M = r.mean()
+                        s = r.std()
+                        U = M + 2*s
+                        L = M - 2*s
+                    else:
+                        window = min(len(ys), 10)
+                        r = s.rolling(window, min_periods=0)
+                        M = r.median()
+                        #U = r.max()
+                        #L = r.min()
+                        U = r.quantile(.9)
+                        L = r.quantile(.1)
+
+                    ax.plot(xs, M, lw=2, c=l.get_color())
+                    ax.fill_between(xs, U, L, color=l.get_color(), alpha=0.25)
 
             ax.set_title(self.name)
             if self.legend:

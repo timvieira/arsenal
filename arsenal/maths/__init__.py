6486711af53091a3f3156924f88bc9b6fc0c825c
timv <tim.f.vieira@gmail.com>
2019-12-16 19:33:04 -0500

    ()

--- arsenal/maths/__init__.py
+++ arsenal/maths/__init__.py
@@ -1,5 +1,5 @@
-#from math import *
 from arsenal.maths.util import *
+from arsenal.maths.rvs import *
 from arsenal.maths.compare import compare, check_equal, pp_plot
-from arsenal.maths.checkgrad import fdcheck, quick_fdcheck
 from arsenal.maths import stepsize
+from arsenal.maths.checkgrad import fdcheck, quick_fdcheck

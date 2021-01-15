a784076d1956789cb179ed6ad2acb42d07f9f6f8
timv <tim.f.vieira@gmail.com>
2021-01-15 12:19:46 -0500

    ()

--- arsenal/maths/stats/permutation_test.py
+++ arsenal/maths/stats/permutation_test.py
@@ -1,18 +1,20 @@
 import numpy as np
 
 
-def mc_perm_test(xs, ys, nmc):
+def mc_perm_test(xs, ys, samples=10000, statistic=np.mean):
+    def effect(xs,ys): return np.abs(statistic(xs) - statistic(ys))
     n, k = len(xs), 0.0
     diff = np.abs(np.mean(xs) - np.mean(ys))
     zs = np.concatenate([xs, ys])
-    for _ in range(nmc):
+    for _ in range(samples):
         np.random.shuffle(zs)
-        k += diff <= np.abs(np.mean(zs[:n]) - np.mean(zs[n:]))
-    return k * 1.0 / nmc
+        k += diff <= effect(zs[:n], zs[n:])
+    return k / samples
 
 
-def mc_paired_perm_test(xs, ys, nmc):
-    """Paired permutation test
+def mc_paired_perm_test(xs, ys, samples=10000, statistic=np.mean):
+    """
+    Paired permutation test
 
     >>> xs = np.array([1,2,3,4,5,6])
     >>> ys = np.array([2,3,4,5,6,7])
@@ -21,21 +23,36 @@ def mc_paired_perm_test(xs, ys, nmc):
 
     Under the unpaired test, we do not have a significant difference because the
     systems only differ in two positions.
-
-    >>> assert u > .40
+      >>> assert u > .40
 
     Under the Paired test, we have more power!
-
-    >>> assert p < .05
+      >>> assert p < .05
 
     """
-    n, k = len(xs), 0.0
-    zs = xs - ys
-    diff = np.abs(np.mean(zs))
-    for _ in range(nmc):
-        signs = np.random.randint(0,2,n) * 2 - 1
-        k += diff <= np.abs(np.mean(signs * zs))
-    return k / nmc
+    def effect(xs,ys): return np.abs(statistic(xs) - statistic(ys))
+    assert len(xs) == len(ys)
+    n, k = len(xs), 0
+    diff = effect(xs,ys)    # observed difference
+    for _ in range(samples):  # for each random sample
+        swaps = np.random.randint(0,2,n).astype(bool)     # flip n coins
+        k += diff <= effect(np.select([swaps,~swaps],[xs,ys]), # swap elements accordingly
+                            np.select([~swaps,swaps],[xs,ys]))
+    return k / samples  # fraction of random samples that achieved at least the observed difference
+
+
+from itertools import product
+def bf_paired_perm_test(xs, ys, statistic=np.mean):
+    def effect(xs,ys): return np.abs(statistic(xs) - statistic(ys))
+    assert len(xs) == len(ys)
+    observed = effect(xs, ys)
+    p = 0.0; n = len(xs)
+    for swaps in product(*([0,1] for _ in range(n))):
+        swaps = np.array(swaps, dtype=bool)
+        pe = 2**-n
+        E = effect(np.select([swaps,~swaps],[xs,ys]), # swap elements accordingly
+                   np.select([~swaps,swaps],[xs,ys]))
+        p += pe * (E >= observed)
+    return p
 
 
 def verbose_paired_perm_test(xs, ys, nmc=1000, threshold=0.05, fmt='%.4f'):

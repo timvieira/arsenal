f31a67d5ca20d4b72d16747ad6626694be49a523
timv <tim.f.vieira@gmail.com>
2024-07-09 10:03:59 -0400

    something weird was going on the pdict so I replaced it

--- arsenal/datastructures/pdict.py
+++ arsenal/datastructures/pdict.py
@@ -1,59 +1,71 @@
-class pdict(dict):
+# This implementation is based on
+# https://code.activestate.com/recipes/522995-priority-dict-a-priority-queue-with-updatable-prio/
 
-    def __init__(self):
-        """
-        Initialize pdict by creating binary heap of pairs (value,key).
-        Note that changing or removing a dict entry will
-        not remove the old pair from the heap until it is found by peek() or
-        until the heap is rebuilt.'
-        """
-        self.__heap = []
-        dict.__init__(self)
+class pdict(dict):
+    """Dictionary that can be used as a priority queue.
+
+    Keys of the dictionary are items to be put into the queue, and values
+    are their respective priorities. All dictionary methods work as expected.
+    The advantage over a standard heapq-based priority queue is
+    that priorities of items can be efficiently updated (amortized O(1))
+    using code as 'thedict[item] = new_priority.'
+
+    The 'smallest' method can be used to return the object with lowest
+    priority, and 'pop_smallest' also removes it.
+
+    The 'sorted_iter' method provides a destructive sorted iterator.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(pdict, self).__init__(*args, **kwargs)
+        self._rebuild_heap()
+
+    def _rebuild_heap(self):
+        self._heap = [(v, k) for k, v in self.items()]
+        heapify(self._heap)
 
     def pop(self):
-        x = self.peek()
-        del self[x]
-        return x
+        return self.popitem()[0]
 
-    def peek(self):
-        "Find smallest item after removing deleted items from heap."
-        if len(self) == 0: raise IndexError("smallest of empty pdict")
-        heap = self.__heap
-        while heap[0][1] not in self or self[heap[0][1]] != heap[0][0]:
-            lastItem = heap.pop()
-            insertionPoint = 0
-            while 1:
-                smallChild = 2*insertionPoint+1
-                if smallChild+1 < len(heap) and heap[smallChild][0] > heap[smallChild+1][0]:
-                    smallChild += 1
-                if smallChild >= len(heap) or lastItem[0] <= heap[smallChild][0]:
-                    heap[insertionPoint] = lastItem
-                    break
-                heap[insertionPoint] = heap[smallChild]
-                insertionPoint = smallChild
-        return heap[0][1]
+    def popitem(self):
+        """Return the item with the lowest priority.
+
+        Raises IndexError if the object is empty.
+        """
+
+        heap = self._heap
+        v, k = heap[0]
+        while k not in self or self[k] != v:
+            heappop(heap)
+            v, k = heap[0]
+        del self[k]
+        return (k, v)
 
     def __setitem__(self, key, val):
-        """
-        Change value stored in dictionary and add corresponding
-        pair to heap.  Rebuilds the heap if the number of deleted items grows
-        too large, to avoid memory leakage.
-        """
-        dict.__setitem__(self, key, val)
-        heap = self.__heap
-        if len(heap) > 2 * len(self):
-            self.__heap = [(v,k) for k,v in self.items()]
-            self.__heap.sort(key=lambda x: x[0])  # builtin sort likely faster than O(n) heapify
-        else:
-            newPair = (val,key)
-            insertionPoint = len(heap)
-            heap.append(None)
-            while insertionPoint > 0 and newPair[0] < heap[(insertionPoint-1)//2][0]:
-                heap[insertionPoint] = heap[(insertionPoint-1)//2]
-                insertionPoint = (insertionPoint-1)//2
-            heap[insertionPoint] = newPair
+        # We are not going to remove the previous value from the heap, since
+        # this would have a cost O(n).
+        super(pdict, self).__setitem__(key, val)
+        heappush(self._heap, (val, key))
 
     def setdefault(self, key, val):
-        "Reimplement setdefault to call our customized __setitem__."
-        if key not in self: self[key] = val
+        if key not in self:
+            self[key] = val
+            return val
         return self[key]
+
+    def update(self, *args, **kwargs):
+        # Reimplementing dict.update is tricky -- see e.g.
+        # http://mail.python.org/pipermail/python-ideas/2007-May/000744.html
+        # We just rebuild the heap from scratch after passing to super.
+
+        super(pdict, self).update(*args, **kwargs)
+        self._rebuild_heap()
+
+    def sorted_iter(self):
+        """Sorted iterator of the priority dictionary items.
+
+        Beware: this will destroy elements as they are returned.
+        """
+
+        while self:
+            yield self.pop_smallest()
